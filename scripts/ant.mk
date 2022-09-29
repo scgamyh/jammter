1975ae586f4129cf2f3aa5174242ae9a9cb15b64
black-pigeon <1530604142@qq.com>
2022-09-29 16:26:32 +0800

    add support for both antsdr-e310 and antsdr-e200

--- scripts/ant.mk
+++ scripts/ant.mk
@@ -2,7 +2,7 @@
 # Target specific constants go here
 
 HDF_URL:=http://github.com/analogdevicesinc/plutosdr-fw/releases/download/${LATEST_TAG}/system_top.hdf
-TARGET_DTS_FILES:= zynq-ant-sdr.dtb zynq-ant-sdr-revb.dtb zynq-ant-sdr-revc.dtb
+TARGET_DTS_FILES:= zynq-ant.dtb 
 COMPLETE_NAME:=ANTSDR
 ZIP_ARCHIVE_PREFIX:=antsdr
 DEVICE_VID:=0x0456
